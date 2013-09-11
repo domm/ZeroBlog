921474440b059f079b42956982b8c9b484e038b4
Thomas Klausner <domm@plix.at>
2013-09-11 21:40:27 +0200

    some 0mq sockopts

--- lib/ZeroBlog/Publisher.pm
+++ lib/ZeroBlog/Publisher.pm
@@ -4,13 +4,17 @@ use Moose;
 use ZMQx::Class;
 use Digest::SHA1 qw(sha1_hex);
 
+use ZMQ::Constants qw(:all);
+
 has 'secret' => (is=>'ro',isa=>'Str',required=>1);
 has 'endpoint' => (is=>'ro',isa=>'Str',required=>1);
 has 'requestor' => (is=>'ro',isa=>'ZMQx::Class::Socket',lazy_build=>1,required=>1);
 sub _build_requestor {
     my $self = shift;
-    return ZMQx::Class->socket( 'REQ', connect => $self->endpoint );
-
+    return ZMQx::Class->socket( 'REQ', connect => $self->endpoint, {
+        rcvtimeo=>500,
+        linger=>0
+    } );
 }
 
 sub send {
@@ -23,11 +27,16 @@ sub send {
     $socket->send([$token, $message]);
     my $rv = $socket->receive(1);
 
-    if ($rv && ref($rv) eq 'ARRAY' && $rv->[0] eq 'ok') {
-        return 1;
+    if ($rv) {
+        if (ref($rv) eq 'ARRAY' && $rv->[0] eq 'ok') {
+            return 1;
+        }
+        else {
+            die $rv;
+        }
     }
     else {
-        die $rv;
+        die "could not connect";
     }
 }
 
