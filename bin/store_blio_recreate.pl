3e5cace0c27e359d7dbf716a5e64cf93c19d2eae
Thomas Klausner <domm@plix.at>
2013-09-29 21:17:13 +0200

    store_recreate with Subscriber

--- bin/store_blio_recreate.pl
+++ bin/store_blio_recreate.pl
@@ -1,7 +1,15 @@
+package StoreBlioRecreate;
+use strict;
+use warnings;
+use FindBin;
+use local::lib "$FindBin::Bin/../local";
+use lib "$FindBin::Bin/../lib";
 use 5.014;
+
 use Moose;
-use ZMQx::Class;
-use AnyEvent;
+extends 'ZeroBlog::Subscriber';
+with 'MooseX::Getopt';
+
 use Path::Class;
 use DBI;
 use DateTime;
@@ -9,22 +17,18 @@ use DateTime;
 my $blio_base = dir('/home/domm/privat/domm.plix.at');
 my $DBH = DBI->connect("dbi:SQLite:dbname=".$blio_base->file('sqlite','microblog.db'),'','');
 
-my $sub = ZMQx::Class->socket( 'SUB', connect => 'tcp://localhost:3334' );
-$sub->subscribe('');
+__PACKAGE__->new_with_options->run(sub {
+    my ($self, $msg) = @_;
+    my ($message) = @$msg;
 
-my $watcher = $sub->anyevent_watcher( sub {
-    while ( my $msg = $sub->receive ) {
-        my ($message) = @$msg;
-        say scalar(localtime)." got $message";
-        $DBH->do(
-            'INSERT INTO microblog (date,message) values (?,?)',undef,
-            DateTime->now(time_zone=>'local')->iso8601,
-            $message
-        );
-        say "rebuilding website";
-        system($blio_base->file('build')->stringify);
-    }
+    say "storing message in DB";
+    $DBH->do(
+        'INSERT INTO microblog (date,message) values (?,?)',undef,
+        DateTime->now(time_zone=>'local')->iso8601,
+        $message
+    );
+    say "rebuilding website";
+    system($blio_base->file('build')->stringify);
 });
-AnyEvent->condvar->recv;
 
 1;
