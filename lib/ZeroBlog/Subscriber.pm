7c26b97024eb54e640060823661356842b66c4ac
Thomas Klausner <domm@plix.at>
2013-09-29 21:16:42 +0200

    perltidy

--- lib/ZeroBlog/Subscriber.pm
+++ lib/ZeroBlog/Subscriber.pm
@@ -4,24 +4,31 @@ use Moose;
 use ZMQx::Class;
 use AnyEvent;
 
-has 'endpoint' => (is=>'ro',isa=>'Str',required=>1);
-has 'subscribe' => (is=>'ro',isa=>'Str',default=>'');
-has 'subscriber' => (is=>'ro',isa=>'ZMQx::Class::Socket',lazy_build=>1,required=>1);
+has 'endpoint'  => ( is => 'ro', isa => 'Str', required => 1 );
+has 'subscribe' => ( is => 'ro', isa => 'Str', default  => '' );
+has 'subscriber' => (
+    is         => 'ro',
+    isa        => 'ZMQx::Class::Socket',
+    lazy_build => 1,
+    required   => 1
+);
+
 sub _build_subscriber {
     my $self = shift;
     return ZMQx::Class->socket( 'SUB', connect => $self->endpoint );
 }
 
 sub run {
-    my ($self, $callback) = @_;
+    my ( $self, $callback ) = @_;
 
     my $subscriber = $self->subscriber;
-    $subscriber->subscribe($self->subscribe);
-    my $watcher = $subscriber->anyevent_watcher( sub {
-        while ( my $msg = $subscriber->receive ) {
-            &$callback($self, $msg);
-        }
-    });
+    $subscriber->subscribe( $self->subscribe );
+    my $watcher = $subscriber->anyevent_watcher(
+        sub {
+            while ( my $msg = $subscriber->receive ) {
+                &$callback( $self, $msg );
+            }
+        } );
     AnyEvent->condvar->recv;
 }
 
